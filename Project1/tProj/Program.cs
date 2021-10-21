2bdd2da5de769b43188ec351f1ec1e1d730b5ac0
CallumStanbury <72077389+CallumStanbury@users.noreply.github.com>
2021-10-21 19:37:02 +0100

    Build Commit

---
+++ Project1/tProj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
