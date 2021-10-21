4677264f12b85b09076eb3cc35125696ade0a80f
CallumStanbury <72077389+CallumStanbury@users.noreply.github.com>
2021-10-21 20:45:02 +0100

    Calculator Push

--- Project1/tProj/Program.cs
+++ Project1/tProj/Program.cs
@@ -6,7 +6,57 @@ namespace tProj
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int N1;
+            int N2;
+            int Op1;
+            int sum;
+            char rerun = 'y';
+
+            do
+            {
+                Console.WriteLine("Please enter a number");
+                N1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter an Operator-\n 1. Addition \n 2. Subtraction \n 3. Division \n 4. Multiplication");
+                Op1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter 2nd number");
+                N2 = int.Parse(Console.ReadLine());
+
+                if (Op1 == 1)
+                {
+                    sum = N1 + N2;
+                    Console.WriteLine("Your Answer is- {0}", sum);
+                }
+
+                if (Op1 == 2)
+                {
+                    sum = N1 - N2;
+                    Console.WriteLine("Your Answer is- {0}", sum);
+                }
+
+                if (Op1 == 3)
+                {
+                    sum = N1 / N2;
+                    Console.WriteLine("Your Answer is- {0}", sum);
+                }
+
+                if (Op1 == 4)
+                {
+                    sum = N1 * N2;
+                    Console.WriteLine("Your Answer is- {0}", sum);
+                }
+
+                if (Op1 >= 5)
+                {
+
+                    Console.WriteLine("Invalid Imput");
+                }
+
+                Console.WriteLine("Do You Want to Use Again? - Y/N");
+                rerun = Console.ReadLine()[0];
+            
+
+            } while (rerun == 'y');
+
         }
     }
 }
